527d131a8cbcf30bd703f2bdf5f2d6e0f671e771
Evan Obenauer <evanjohnobenauer@gmail.com>
2026-02-08 14:01:14 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Trade Scavenger
+
+TradeScavenger is the planned successor to TradeCompanion.
+It will be a backtesting tool. With any provided data, it will be able
+to test any strategy
+
+
---
+++ src/main/java/ejo/tradescavenger/Main.java
@@ -0,0 +1,10 @@
+package ejo.tradescavenger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static void main() {
+        System.out.println("Trade Scavenger! YAY");
+    }
+}
