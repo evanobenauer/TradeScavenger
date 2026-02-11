2bf4d8a650c38f1222fcfb47feb4e6fc241995e1
Evan Obenauer <evanjohnobenauer@gmail.com>
2026-02-10 20:17:46 -0500

    Accidentally left debug features in the main method oops

--- src/main/java/ejo/tradescavenger/App.java
+++ src/main/java/ejo/tradescavenger/App.java
@@ -13,24 +13,10 @@ public class App {
 
     static void main() {
         FileUtil.createFolderPath("stock_data"); //Create the stock_data folder
-
-        HistoricalDataContainer container = new HistoricalDataContainer("stock_data","SPY_1min") {
-            @Override
-            public float[] getNullData() {
-                return new float[]{-1,-1,-1,-1,-1};
-            }
-        };
-
-        container.load(new DateTime(2022,1,1,0,0,0),new DateTime(2022,11,1,0,0,0));
-
-        for (long str : container.getLoadedData().keySet()) {
-            System.out.println(str);
-        }
-
-        //WINDOW.setScene(new MainMenuScene());
-        //WINDOW.initAntiAliasingLevel(4);
-        //WINDOW.init();
-        //WINDOW.startThreadTickLoop();
-        //WINDOW.runMainRenderLoop();
+        WINDOW.setScene(new MainMenuScene());
+        WINDOW.initAntiAliasingLevel(4);
+        WINDOW.init();
+        WINDOW.startThreadTickLoop();
+        WINDOW.runMainRenderLoop();
     }
 }
